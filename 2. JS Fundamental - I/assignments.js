9f826d8a9d03724618a24d3ced69f6d2f4367fd8
Shabd Kumar <shabdkumarsahu@gmail.com>
2022-08-02 11:53:47 +0530

    Added DataType

--- 2. JS Fundamental - I/assignments.js
+++ 2. JS Fundamental - I/assignments.js
@@ -1,6 +1,14 @@
-let country = "India";
-let continent = "Asia";
-let population = "1,417 M";
-console.log("Country Name: " + country);
-console.log("Continent Name: " + continent);
-console.log("Population of " + country + ": " + population);
+/* Values and Variables
+1. Declare variables called 'country', 'continent' and 'population' and
+assign their values according to your own country(population in millions)
+2. Log their values to the console */
+
+// console.log("Assessment: Values and Variables");
+// console.log("--------------------------------")
+
+// let country = "India";
+// let continent = "Asia";
+// let population = "1,417 M";
+// console.log("Country Name: " + country);
+// console.log("Continent Name: " + continent);
+// console.log("Population of " + country + ": " + population);
--- 2. JS Fundamental - I/script.js
+++ 2. JS Fundamental - I/script.js
@@ -14,15 +14,45 @@ let firstName = "Shabd";
 console.log(firstName);
 console.log(firstName);
 
+// Variable Conventions
 let firstPersonName = "Akash";
 let $first = "kumar";
 let name = "Kumar Akash";
 let PI = 3.1415;
 let first123 = "Nikki";
-
 let firstJob = "Associate Software Engineer";
 let currentJob = "FrontEndDeveloper";
 let job1 = "Associate Software Engineer";
 let job2 = "FrontEndDeveloper";
 
 console.log(firstJob);
+
+
+// Data Types
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+//boolean type
+console.log(typeof javascriptIsFun);
+console.log(typeof true);
+//String type
+console.log(typeof "true");
+//number type
+console.log(typeof 35);
+
+//dynamic type
+console.log(typeof javascriptIsFun);
+javascriptIsFun = "Yes!";
+console.log(typeof javascriptIsFun);
+
+//undefined type
+let year;
+console.log(year);
+console.log(typeof year);
+year = 1995;
+console.log(typeof year);
+
+//null type
+console.log(null);
+console.log(typeof null); //bug in javascript
