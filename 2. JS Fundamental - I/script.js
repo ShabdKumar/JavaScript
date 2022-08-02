7075fbce299699ffe5fa5fefeb32ba4cb6d5d0ca
Shabd Kumar <shabdkumarsahu@gmail.com>
2022-08-02 14:41:29 +0530

    Added Operator Precedence

--- 2. JS Fundamental - I/script.js
+++ 2. JS Fundamental - I/script.js
@@ -1,15 +1,15 @@
 let js = "amazing";
 // if (js == 'amazing') alert("JavaScript is Fun!");
-console.log(40 + 30 - 29);
+console.log(40 + 30 - 29); // 41
 let result = 40 + 30 - 29 ;
-console.log(result);
-console.log(js);
+console.log(result); // 41
+console.log(js); // amazing
 
 /* Values and Variables: */
 
 let firstName = "Shabd";
-console.log(firstName);
-console.log(firstName);
+console.log(firstName); // Shabd
+console.log(firstName); // Shabd
 
 // Variable Conventions
 let firstPersonName = "Akash";
@@ -21,57 +21,57 @@ let firstJob = "Associate Software Engineer";
 let currentJob = "FrontEndDeveloper";
 let job1 = "Associate Software Engineer";
 let job2 = "FrontEndDeveloper";
-console.log(firstJob);
+console.log(firstJob); // Associate Software Engineer
 
 /* Data Types: */
 
 let javascriptIsFun = true; 
-console.log(javascriptIsFun);
+console.log(javascriptIsFun); // true
 //boolean type
-console.log(typeof javascriptIsFun);
-console.log(typeof true);
+console.log(typeof javascriptIsFun); // boolean
+console.log(typeof true); // boolean
 //String type
-console.log(typeof "true");
+console.log(typeof "true"); // string
 //number type
-console.log(typeof 35);
+console.log(typeof 35); // number
 
 //dynamic type
-console.log(typeof javascriptIsFun);
+console.log(typeof javascriptIsFun); // boolean
 javascriptIsFun = "Yes!";
-console.log(typeof javascriptIsFun);
+console.log(typeof javascriptIsFun); // string
 
 //undefined type
 let year;
-console.log(year);
-console.log(typeof year);
+console.log(year); // undefined
+console.log(typeof year); // undefined
 year = 1995;
-console.log(typeof year);
+console.log(typeof year); // number
 
 //null type
-console.log(null);
-console.log(typeof null); //bug in javascript: showing object type
+console.log(null); // null
+console.log(typeof null); // object /*bug in javascript: showing object type*/
 
 /* let, const & var keywords: */
 
 //let keyword
 let age = 30;
 age = 31;
-console.log(age);
+console.log(age); // 31
 
 //const keyword
 const birthYear = 1995;
-console.log(birthYear);
+console.log(birthYear); // 1995
 //birthYear = 2000; //Uncaught TypeError: Assignment to constant variable
 //const birthYear1; //Uncaught SyntaxError: Missing initializer in const declaration
 
 //var keyword
 var job = "Engineer";
 job = "Unemployed";
-console.log(job);
+console.log(job); // Unemployed
 
 //without declaration
 lastName = "Sahu";
-console.log(lastName);
+console.log(lastName); // Sahu
 
 /* Basic Operators: */
 
@@ -102,3 +102,16 @@ console.log(ageAkash >= 21); // true
 const isMarriageAge = ageAkash >= 21;
 console.log(isMarriageAge); // true
 console.log(currentYear - 1995 > currentYear - 1999); // true
+
+/* Operator Precedence: */
+
+console.log(currentYear - 1995 > currentYear - 1999); // true
+
+let y, z;
+y = z = 25 - 10 - 5; /* y = z = 10; y = 10; */
+console.log(y, z); // 10 10
+
+let averageAge = ageShabd + ageAkash / 2;
+console.log(averageAge); // 38.5
+averageAge = (ageShabd + ageAkash) / 2;
+console.log(averageAge); // 25
